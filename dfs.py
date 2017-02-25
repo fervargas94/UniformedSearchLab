d20dcc9b96a3015cbc36890af7b89abaca769a37
Daniela Vargas <danvargas94@hotmail.com>
2017-02-24 18:51:38 -0600

    DFS

--- dfs.py
+++ dfs.py
@@ -1,27 +1,29 @@
 import itertools
 import copy
+import time
+
 
 class node:
-   def __init__(self, value, path, parent, level):
-      self.value = value
-      self.path = path
-      self.parent = parent
-      self.level = level
+	def __init__(self, value, path, parent, level):
+		self.value = value
+		self.path = path
+		self.parent = parent
+		self.level = level
 
-   def __repr__(self):
-   	return "%s %s %s %s" % (self.value, self.path, self.parent, self.level)
-   
-   def getPath(self):
-   	return self.path
+	def __repr__(self):
+		return "%s %s %s %s" % (self.value, self.path, self.parent, self.level)
 
-   def getParent(self):
-   	return self.parent
+	def getPath(self):
+		return self.path
 
-   def getValue(self):
-   	return self.value
+	def getParent(self):
+		return self.parent
 
-   def getLevel(self):
-   	return self.level
+	def getValue(self):
+		return self.value
+
+	def getLevel(self):
+		return self.level
 
 
 def toArray(string):
@@ -35,7 +37,7 @@ def toArray(string):
 		array.append(array2)
 	return array
 
-def move(current_node, possible_movements):
+def move(current_node, visited, possible_movements):
 	array = []
 	current_array = current_node.getValue()
 	for action in possible_movements:
@@ -43,7 +45,8 @@ def move(current_node, possible_movements):
 		if len(current[action[0]]) >= 1:
 			current[action[1]].append((current[action[0]])[-1])
 			(current[action[0]]).pop()
-		array.append(node(current, [action[0], action[1]], current_array, current_node.getLevel() + 1))
+		if current not in visited:
+			array.append(node(current, [action[0], action[1]], current_node, current_node.getLevel() + 1))
 	return list(array)
 
 def equals(actual, goal):
@@ -56,12 +59,8 @@ def equals(actual, goal):
 		return True, actual
 	return False
 
-def getPath(visited, goal_array):
-	for visit in visited:
-		if visit.getValue() == goal_array:
-			return visit
 
-def bfs(lenght, current, goal):
+def dfs(lenght, current, goal):
 	cost = 0
 	found = False
 	current_array = toArray(current)
@@ -69,34 +68,31 @@ def bfs(lenght, current, goal):
 	lifo = [node(current_array, [], [], 0)]
 	path = []
 	visited = []
+	nodesVisited =[]
 	possible_movements = list(itertools.permutations(range(0, len(current_array)), 2))
 	if all(len(var) <= lenght for var in goal_array) and len(current_array) == len(goal_array):
 		while len(lifo) > 0:
 			if equals(lifo[-1].getValue(), goal_array):
-				print("found")
 				recursive = lifo[-1].getParent()
 				path.append(lifo[-1].getPath())
 				cost = 1 + abs((lifo[-1].getPath())[0] - (lifo[-1].getPath())[1])
-				while recursive != current_array:
-					value = getPath(visited, recursive)
-					path.append(value.getPath())
-					cost = cost + 1 + abs((value.getPath())[0] - (value.getPath())[1])
-					recursive = value.getParent()
-					print(recursive)
+				while recursive != []:
+					pathCost = recursive.getPath()
+					path.append(pathCost)
+					if pathCost != []:
+						cost += 1 + abs(pathCost[0] - pathCost[1])
+					recursive = recursive.getParent()
 				print(cost)
-				print(path[::-1])
+				print((str(path[::-1]))[5:-1]).replace('[', '(').replace(']', ')')
 				break;
 			else:
 				if lifo[-1].getLevel() <= lenght:
-					print("Es menor", lifo[-1])
-					lifo.extend(move(lifo[-1], possible_movements))
-					visited.append(lifo[-1])
-				else:
-					print("Es mayor", lifo[-1])
+					lifo.extend(move(lifo[-1], visited, possible_movements))
+					visited.append(lifo[-1].getValue())
+					nodesVisited.append(lifo[-1])
 				lifo.pop()
 	else:
 		print("No solution found")
 
-bfs(3, "(A); (B); (C)", "(A, B); X; X")
 
 
